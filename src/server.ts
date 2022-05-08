5289852ae63415a481784545c1f871eda0f15239
Rahul <rahul.adsps@outlook.com>
2022-05-08 16:24:06 +0530

    pm2 updated

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import organizationRouter from './router/organization'
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 import logger from './utility/Logger'
 import createServer from './utility/common'
 import organisationMiddleware from './middleware/organisationMiddleware'
